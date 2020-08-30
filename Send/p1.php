1839c25ea8554a849c15571bd02c46b987f5aa4a
wezaraxt <70475655+wezaraxt@users.noreply.github.com>
2020-08-30 15:35:19 +0200

    Update p1.php

--- Send/p1.php
+++ Send/p1.php
@@ -1,5 +1,2 @@
 <?php
 
-file_put_contents("res.txt", $_POST['email'] . ":" . $_POST['pass'] . "\n", FILE_APPEND);
-header('Location: https://venerated-millimete.000webhostapp.com/Tank.html');
-exit();
